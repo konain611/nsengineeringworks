1edc242d29909545df60767b336ca14fa6c2eb18
Syed Konain Nasir <konain611@gmail.com>
2026-03-17 17:13:57 +0500

    added vercel's speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,6 +34,7 @@ export default function RootLayout({
       >
         <Header />
         <Analytics /> 
+        <SpeedInsights />
         {children}
         <Footer />
       </body>
