41f8fbf285f64d519b7c3b4594905356d45778a1
Syed Konain Nasir <konain611@gmail.com>
2026-09-15 23:55:55 +0500

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        primary: 'var(--primary)',
+        card: 'var(--card)',
+      }
+    }
+  },
+  plugins: [],
+}
